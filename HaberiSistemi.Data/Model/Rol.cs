81412c96ce86642cf120c7c959f0b762d3aa1a1e
huseyincamci <hcamci.ce@gmail.com>
2017-05-05 16:20:53 +0300

    Rol modeli eklendi.

---
+++ HaberiSistemi.Data/Model/Rol.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HaberiSistemi.Data.Model
+{
+    [Table("Rol")]
+    public class Rol
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "Rol Adı")]
+        [MinLength(3, ErrorMessage = "Lütfen 3 karakterden fazla değer giriniz."), MaxLength(150, ErrorMessage = "Lütfen 150 karakterden fazla girmeyiniz.")]
+        public string RolAdi { get; set; }
+    }
+}
