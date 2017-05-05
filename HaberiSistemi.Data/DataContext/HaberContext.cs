eee5d390487182761cc31d08e1df2ce6087a4946
huseyincamci <hcamci.ce@gmail.com>
2017-05-05 16:30:10 +0300

    Context içerisine Kullanici ve Rol için DbSet eklendi.

--- HaberiSistemi.Data/DataContext/HaberContext.cs
+++ HaberiSistemi.Data/DataContext/HaberContext.cs
@@ -1,8 +1,11 @@
-﻿using System.Data.Entity;
+﻿using HaberiSistemi.Data.Model;
+using System.Data.Entity;
 
 namespace HaberiSistemi.Data.DataContext
 {
     public class HaberContext : DbContext
     {
+        public DbSet<Kullanici> Kullanicis { get; set; }
+        public DbSet<Rol> Rols { get; set; }    
     }
 }
--- HaberiSistemi.Data/Model/Kullanici.cs
+++ HaberiSistemi.Data/Model/Kullanici.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace HaberiSistemi.Data.Model
 {
+    [Table("Kullanici")]
     public class Kullanici
     {
         [Key]
