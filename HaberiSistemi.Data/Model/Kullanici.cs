a66f697d9b54f1cac0717c5a469334b8cab2cd9c
huseyincamci <hcamci.ce@gmail.com>
2017-05-05 16:16:09 +0300

    Kullanici modeli oluşturuldu.

---
+++ HaberiSistemi.Data/Model/Kullanici.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HaberiSistemi.Data.Model
+{
+    public class Kullanici
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string AdSoyad { get; set; }
+        public string Email { get; set; }
+        public string Sifre { get; set; }
+    }
+}
