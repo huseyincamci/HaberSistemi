62f6c0f31440bcffae3dd84676fadbad640e7bb6
huseyincamci <hcamci.ce@gmail.com>
2017-05-05 18:25:12 +0300

    IOC - Repositoryler eklendi

--- HaberSistemi.Admin/Class/BootStrapper.cs
+++ HaberSistemi.Admin/Class/BootStrapper.cs
@@ -1,5 +1,7 @@
 ﻿using Autofac;
 using Autofac.Integration.Mvc;
+using HaberSistemi.Core.Infrastructure;
+using HaberSistemi.Core.Repository;
 using System.Web.Mvc;
 
 namespace HaberSistemi.Admin.Class
@@ -14,6 +16,10 @@ namespace HaberSistemi.Admin.Class
         private static void BuildAutoFact()
         {
             var builder = new ContainerBuilder();
+            builder.RegisterType<HaberRepository>().As<IHaberRepository>();
+            builder.RegisterType<ResimRepository>().As<IResimRepository>();
+            builder.RegisterType<KullaniciRepository>().As<IKullaniciRepository>();
+            builder.RegisterType<RolRepository>().As<IRolRepository>();
             builder.RegisterControllers(typeof(MvcApplication).Assembly);
 
             var container = builder.Build();
