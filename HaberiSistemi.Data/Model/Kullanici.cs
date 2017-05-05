56ccb0a6cb288fd71f7a51c4c8b0091e9b988e99
huseyincamci <hcamci.ce@gmail.com>
2017-05-05 16:26:52 +0300

    Kullanici modeline attributeler eklendi. Kayıt tarihi, aktiflik ve rol property leri eklendi.

--- HaberiSistemi.Data/Model/Kullanici.cs
+++ HaberiSistemi.Data/Model/Kullanici.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace HaberiSistemi.Data.Model
 {
@@ -7,8 +8,26 @@ namespace HaberiSistemi.Data.Model
         [Key]
         public int Id { get; set; }
 
+        [Display(Name = "Ad Soyad")]
+        [MaxLength(50, ErrorMessage = "Lütfen 50 karakterden fazla değer girmeyiniz.")]
         public string AdSoyad { get; set; }
+
+        [Required]
+        [Display(Name = "E-mail")]
         public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Şifre")]
+        [DataType(DataType.Password)]
+        [MaxLength(16, ErrorMessage = "Lütfen 16 karakterden fazla değer girmeyiniz.")]
         public string Sifre { get; set; }
+
+        [Display(Name = "Kayıt Tarihi")]
+        public DateTime KayitTarihi { get; set; }
+
+        [Display(Name = "Aktif")]
+        public bool Aktif { get; set; }
+
+        public virtual Rol Rol { get; set; }
     }
 }
