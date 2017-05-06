09577095b6639b8d2edc83685e8c78d34313421a
huseyincamci <hcamci.ce@gmail.com>
2017-05-06 12:05:19 +0300

    LoginFilter oluşturuldu.

---
+++ HaberSistemi.Admin/CustomFilter/LoginFilter.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace HaberSistemi.Admin.CustomFilter
+{
+    public class LoginFilter : FilterAttribute, IActionFilter
+    {
+        public void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var session = filterContext.HttpContext.Session?["KullaniciEmail"];
+            if (session != null)
+            {
+                filterContext.Result = new RedirectToRouteResult(
+                    new RouteValueDictionary { { "controller", "Account" }, { "action", "Login" } });
+            }
+        }
+
+        public void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
