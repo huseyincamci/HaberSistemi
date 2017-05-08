4f1ef7ee921f76766d9ae3e750230064fe253ac9
huseyincamci <hcamci.ce@gmail.com>
2017-05-08 14:41:13 +0300

    Kategoriden kullanıcı bilgisi çıkarıldı.

--- HaberiSistemi.Data/Model/Kategori.cs
+++ HaberiSistemi.Data/Model/Kategori.cs
@@ -16,10 +16,6 @@ namespace HaberiSistemi.Data.Model
         [MinLength(2, ErrorMessage = "{0} karakterden az olamaz."), MaxLength(150, ErrorMessage = "150 karakterden fazla girmeyiniz.")]
         public string Url { get; set; }
 
-        public virtual ICollection<Haber> Habers { get; set; }
-
-        [ForeignKey("Kullanici")]
-        public int? KullaiciId { get; set; } 
-        public virtual Kullanici Kullanici { get; set; }    
+        public virtual ICollection<Haber> Habers { get; set; }   
     }
 }
